25f52aaacb2ecc06d44f5ce406572073a1c40911
xfnw <xfnw@ttm.sh>
2020-11-14 23:21:23 +0000

    keep peers file clean

--- ping.sh
+++ ping.sh
@@ -9,15 +9,21 @@ source config.sh
 peers="$(sort realpeers.txt | uniq | awk '{if($1!="FORWARD"){print $1}}' | tr '\n' ',')"
 
 
-for fn in "$(sort peers.txt | uniq)"; do
+for fn in "$(awk '{print $1}' peers.txt)"; do
 
 
-		url=$(echo "$fn" | awk '{print $1}')
-
-		curl -m 1 -L -A "xfnwmesh/1.0" -F "callback=$callback" -F "peers=$peers" $url >/dev/null 2>/dev/null && echo "$url" >> realpeers.txt
+		url="$fn"
+		echo "pinging $url"
+		curl -m 1 -L -A "xfnwmesh/1.0" -F "callback=$callback" -F "peers=$peers" "$url" >/dev/null 2>/dev/null && echo "$url" >> realpeers.txt
 
 
 done
 
+sort peers.txt | uniq > peers.txt.m
+tee peers.txt <peers.txt.m > /dev/null
 
+sort realpeers.txt | uniq > realpeers.txt.m
+tee realpeers.txt <realpeers.txt.m > /dev/null
+
+rm realpeers.txt.m peers.txt.m
 
